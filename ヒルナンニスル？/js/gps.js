b07f999a09a6c7e936b0cd97123a875d4a440821
park19 <park19950@gmail.com>
2016-06-19 14:58:49 +0900

    マップルート表示テスト

--- ヒルナンニスル？/js/gps.js
+++ ヒルナンニスル？/js/gps.js
@@ -29,15 +29,20 @@ function getAdrs() {
             for (var i = adrsData.length - 3; i > 0; i--) {
                 address += adrsData[i].long_name;
             }
-            document.getElementById("address").setAttribute("value", address);
+            document.getElementById("address").value = address;
         } else {
             alert(status + " : ジオコードに失敗しました");
         }
     });
 }
 
-function getLatLng(address) {
-    address = document.getElementById("address").getAttribute("value");
+function getLatLng() {
+    // 入力住所取得
+    var address = document.getElementById("address").value;
+    if(!address || address.match("^\[ 　]")) {
+        alert("住所を入力してください");
+        return;
+    }
 
     //ジオコードオブジェクト
     var geocoder = new google.maps.Geocoder();
@@ -48,8 +53,12 @@ function getLatLng(address) {
         if (status == google.maps.GeocoderStatus.OK) {
             gLat_gps = results[0].geometry.location.lat();
             gLng_gps = results[0].geometry.location.lng();
+
+            // 緯度経度をセット
             document.getElementById("pos").setAttribute("value", gLat_gps+","+gLng_gps);
             document.forms.gpsform.submit();
+        } else if (status == google.maps.GeocoderStatus.ZERO_RESULTS) {
+            alert(address + "は見つかりませんでした。");
         }
     });
 }
--- ヒルナンニスル？/js/map.js
+++ ヒルナンニスル？/js/map.js
@@ -1,19 +1,62 @@
+var map, directionsService;
+var center;
 function initialize() {
     // GSP位置
     var query = window.location.search.toQueryParams();
-    var latlng = query["pos"].split(",");
+    var gpslatlng = query["pos"].split(",");
     var address = query["address"];
-    var center = new google.maps.LatLng(latlng[0],latlng[1]);
-    var mapOptions = {
+    center = new google.maps.LatLng(gpslatlng[0],gpslatlng[1]);
+    //オプション設定
+    var myOptions = {
         zoom: 17,
         center: center,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        scaleControl: true,
     };
-    var map = new google.maps.Map(document.getElementById('map_canvas'),mapOptions);
-    var station = new google.maps.LatLng(35.68140,139.76607);
-    var marker = new google.maps.Marker({
-        position: center,
-        map: map,
-        title: address
+    directionsService = new google.maps.DirectionsService();
+    var directionsDisplay;
+    //マップを描画
+    var mapdiv = document.getElementById('map_canvas');
+    map = new google.maps.Map(mapdiv,myOptions);
+}
+function getPlace(address) {
+    if (address) {
+        var service = new google.maps.places.PlacesService(map);
+        var placeRequest = {
+            //入力したテキスト
+            query: address,
+        }
+        //リクエストを送ってあげるとプライス情報を格納したオブジェクトを返してくれます。
+        service.textSearch(placeRequest, function(results, status) {
+            var places = results[0];
+            toGeocode(places);
+        });
+    }
+}
+function toGeocode(places) {
+    //取得したplacesオブジェクトから緯度と経度をgeocodeとして渡します。
+    var tolatlng = new google.maps.LatLng(places.geometry.location.lat(),places.geometry.location.lng());
+    //ルート取得
+    getRoute(tolatlng);
+}
+//ルート描画用
+function getRoute(latlng) {
+    var request = {
+        //入力地点の緯度、経度
+        origin: center,
+        //到着地点の緯度、経度
+        destination: latlng,
+        //ルートの種類
+        travelMode: google.maps.DirectionsTravelMode.WALKING
+    }
+    directionsService.route(request, function(result, status) {
+        toRender(result);
     });
+}
+function toRender(result) {
+    directionsDisplay = new google.maps.DirectionsRenderer();
+    //取得した情報をセット
+    directionsDisplay.setDirections(result);
+    //マップに描画
+    directionsDisplay.setMap(map);
 }
