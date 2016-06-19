4bca1a90368ae000d8ae23e6e68e6857a80cf2f4
park19 <park19950@gmail.com>
2016-06-19 13:44:29 +0900

    new

---
+++ ヒルナンニスル？/js/gps.js
@@ -0,0 +1,55 @@
+var gLat_gps, gLng_gps;
+function gps() {
+    if (navigator.geolocation) {
+        // 現在の位置情報取得を実施
+        navigator.geolocation.getCurrentPosition(// 位置情報取得成功時
+        function(pos) {
+            gLat_gps = pos.coords.latitude;
+            gLng_gps = pos.coords.longitude;
+            getAdrs();
+        }, function(pos) {
+            // 位置情報取得失敗時
+            alert("位置情報を有効にしてください");
+        });
+    } else {
+        window.alert("本ブラウザではGeolocationが使えません");
+    }
+}
+
+function getAdrs() {
+
+    var latlng = new google.maps.LatLng(gLat_gps,gLng_gps);
+    var gc = new google.maps.Geocoder();
+    gc.geocode({
+        location: latlng
+    }, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            var adrsData = results[0].address_components;
+            var address = "";
+            for (var i = adrsData.length - 3; i > 0; i--) {
+                address += adrsData[i].long_name;
+            }
+            document.getElementById("address").setAttribute("value", address);
+        } else {
+            alert(status + " : ジオコードに失敗しました");
+        }
+    });
+}
+
+function getLatLng(address) {
+    address = document.getElementById("address").getAttribute("value");
+
+    //ジオコードオブジェクト
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({
+        'address': address,
+        'region': 'jp'
+    }, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            gLat_gps = results[0].geometry.location.lat();
+            gLng_gps = results[0].geometry.location.lng();
+            document.getElementById("pos").setAttribute("value", gLat_gps+","+gLng_gps);
+            document.forms.gpsform.submit();
+        }
+    });
+}
---
+++ ヒルナンニスル？/js/map.js
@@ -0,0 +1,27 @@
+function initialize() {
+    // GSP位置
+    var query = window.location.search.toQueryParams();
+    var latlng = query["pos"].split(",");
+    var address = query["address"];
+    var center = new google.maps.LatLng(latlng[0],latlng[1]);
+    var mapOptions = {
+        zoom: 17,
+        center: center,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById('map_canvas'),mapOptions);
+    var station = new google.maps.LatLng(35.68140,139.76607);
+    var marker = new google.maps.Marker({
+        position: center,
+        map: map,
+        title: address
+    });
+    var pos = new google.maps.LatLng(35.70964,139.81074);
+    //  var image = 'img/ico_lopan.png';
+    var lopanMarker = new google.maps.Marker({
+        position: pos,
+        map: map,
+        //    icon: image,
+        title: 'Lopan cafe'
+    });
+}
